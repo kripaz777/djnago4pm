2b8953355417916731e70e7ee7c6a4c5380c4e8c
Aayush <aceraayush@gmail.com>
2024-01-25 16:56:43 +0545

    urls and function added

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,9 @@ from django.urls import path,include
 urlpatterns = [
     path("",home,name = "home"),
     path("about",about,name = "about"),
+    path("contact",contact,name = "contact"),
+    path("portfolio",portfolio,name = "portfolio"),
+    path("price",price,name = "price"),
+    path("services",services,name = "services"),
+
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,15 @@ def home(request):
 
 def about(request):
     return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def portfolio(request):
+    return render(request,'portfolio.html')
+
+def price(request):
+    return render(request,'price.html')
+
+def services(request):
+    return render(request,'services.html')
