4d6f4f7737824334011466555aef48a15e197db5
Aayush <aceraayush@gmail.com>
2024-01-30 16:45:58 +0545

    image view

--- home/models.py
+++ home/models.py
@@ -5,6 +5,8 @@ class AboutMe(models.Model):
     name = models.CharField(max_length=300)
     description = models.TextField()
     image = models.ImageField(upload_to='media')
+    small_description = models.TextField(blank = True)
+    image1 = models.ImageField(upload_to='media',null = True)
 
     def __str__(self):
         return self.name
